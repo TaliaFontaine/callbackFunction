9571bd03c81a0d436fb9433d76c9697d64bfa432
TaliaFontaine <taliafontaine1@gmail.com>
2019-02-07 20:17:03 +0000

    forEach function on callback

---
+++ callbackforEach.js
@@ -0,0 +1,12 @@
+function findWaldo(arr, found) {
+    arr.forEach(function(item, index) {
+        if (item === "Waldo") {
+         found(index);
+    }
+  });
+}
+function actionWhenFound(i) {
+  console.log("Found Waldo at index " + i + "!");
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
